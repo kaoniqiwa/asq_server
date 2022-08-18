c67f375f7d6560e5d4c2428e5d0e71cb0eb4b530
panminxiang <1061886912@qq.com>
2022-08-18 13:26:39 +0800

    pmx

--- backend/company.php
+++ backend/company.php
@@ -1,5 +1,6 @@
 <?php
 include('../utility/util.php');
+include('./GUID.php');
 
 $Data = array();
 
@@ -9,27 +10,27 @@ if (!isset($data->flow)) {
   die('Operation Denied!');
 }
 
-if ($data->flow == 'list_company') {
-  $PageSize = $data->PageSize;
-  $PageIndex = $data->PageIndex;
-  $Name = '';
-  if (isset($data->Name)) {
-    $Name = $data->Name;
+if ($data->flow == 'listCompany') {
+  $pageSize = $data->pageSize;
+  $pageIndex = $data->pageIndex;
+  $name = '';
+  if (isset($data->name)) {
+    $name = $data->name;
   }
-  $Start = ($PageIndex - 1) * $PageSize;
+  $start = ($pageIndex - 1) * $pageSize;
 
 
+  $sql = " select id,guid,name,account_name,asq_total,asq_left,asq_se_total,asq_se_left,asq_se_2_total,asq_se_2_left from company  where name like '%$name%'  or account_name like '%$name%' limit $start,$pageSize";
 
-  $sql = " select * from company  where name like '%$Name%'  or a_name like '%$Name%' limit $Start,$PageSize";
-
-  $TotalRecortCount = $conn->query("select count(*) from company  where name like '%$Name%'")->fetch_assoc()['count(*)'];
+  $totalRecortCount = $conn->query("select count(*) from company  where name like '%$name%'")->fetch_assoc()['count(*)'];
 
 
-  $TotalRecortCount = intval($TotalRecortCount);
-  $PageCount  = ceil($TotalRecortCount / $PageSize);
+  $totalRecortCount = intval($totalRecortCount);
+  $PageCount  = ceil($totalRecortCount / $pageSize);
   $RecordCount = 0;
 
   $result = $conn->query($sql);
+
   if ($conn->affected_rows != 0) {
     $RecordCount = $conn->affected_rows;
     while ($rs = $result->fetch_assoc()) {
@@ -40,18 +41,42 @@ if ($data->flow == 'list_company') {
 
   echo json_encode(
     array(
-      "FaultCode" => 0,
-      'FaultReason' => 'OK',
-      "Data" => [
-        "Data" => $Data,
-        "Page" => array(
-          "PageCount" => $PageCount,
-          "PageSize" => $PageSize,
-          "PageIndex" => $PageIndex,
-          "RecordCount" => $RecordCount,
-          "TotalRecordCount" => $TotalRecortCount
+      "faultCode" => 0,
+      'faultReason' => 'OK',
+      "data" => [
+        "data" => $Data,
+        "page" => array(
+          "pageCount" => $PageCount,
+          "pageSize" => $pageSize,
+          "pageIndex" => $pageIndex,
+          "recordCount" => $RecordCount,
+          "totalRecordCount" => $totalRecortCount
         )
       ]
     )
   );
+} else if ($data->flow == 'addCompany') {
+  $guid = GUID();
+
+  $name = $data->name;
+  $account_name = $data->account_name;
+  $account_pass = $data->account_pass;
+  $asq_total = $data->asq_total;
+  $asq_left = $data->asq_left;
+  $asq_se_total = $data->asq_se_total;
+  $asq_se_left = $data->asq_se_left;
+  $asq_se_2_total  = $data->asq_se_2_total;
+  $asq_se_2_left  = $data->asq_se_2_left;
+
+  $sql = "insert into company (guid,name,account_name,account_pass,asq_total,asq_left,asq_se_total,asq_se_left,asq_se_2_total,asq_se_2_left) values ('$guid','$name','$account_name','$account_pass',$asq_total,$asq_left,$asq_se_total,$asq_se_left,$asq_se_2_total,$asq_se_2_left)";
+
+  $result =   $conn->query($sql);
+  if ($conn->affected_rows != 0) {
+    echo json_encode(
+      [
+        "FaultCode" => 0,
+        'FaultReason' => 'OK',
+      ]
+    );
+  }
 }
