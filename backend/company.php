1304b83628b828960a3857db3a2c4ca162977dbb
panminxiang <1061886912@qq.com>
2022-08-19 14:25:00 +0800

    pmx

--- backend/company.php
+++ backend/company.php
@@ -4,23 +4,23 @@ include('./GUID.php');
 
 $Data = array();
 
-$data = json_decode(file_get_contents('php://input'));
+$input = json_decode(file_get_contents('php://input'));
 
-if (!isset($data->flow)) {
+if (!isset($input->flow)) {
   die('Operation Denied!');
 }
 
-if ($data->flow == 'listCompany') {
-  $pageSize = $data->pageSize;
-  $pageIndex = $data->pageIndex;
+if ($input->flow == 'listCompany') {
+  $pageSize = $input->pageSize;
+  $pageIndex = $input->pageIndex;
   $name = '';
-  if (isset($data->name)) {
-    $name = $data->name;
+  if (isset($input->name)) {
+    $name = $input->name;
   }
   $start = ($pageIndex - 1) * $pageSize;
 
 
-  $sql = " select id,guid,name,account_name,asq_total,asq_left,asq_se_total,asq_se_left,asq_se_2_total,asq_se_2_left from company  where name like '%$name%'  or account_name like '%$name%' limit $start,$pageSize";
+  $sql = "select id,name,account_name,account_pass,asq_total,asq_left,asq_se_total,asq_se_left,asq_se_2_total,asq_se_2_left from company  where name like '%$name%'  or account_name like '%$name%' limit $start,$pageSize";
 
   $totalRecortCount = $conn->query("select count(*) from company  where name like '%$name%'")->fetch_assoc()['count(*)'];
 
@@ -37,6 +37,21 @@ if ($data->flow == 'listCompany') {
       array_push($Data, $rs);
     }
   }
+
+
+  foreach ($Data as &$value) {
+    $id = $value['id'];
+    $value['sub_accounts'] = [];
+    $sql = "select id,name,level,dept from sub_accounts where c_id ='$id'";
+    $result =  $conn->query($sql);
+    if ($conn->affected_rows != 0) {
+      while ($rs = $result->fetch_assoc()) {
+        array_push($value['sub_accounts'], $rs);
+      }
+    }
+  }
+
+
   $conn->close();
 
   echo json_encode(
@@ -55,28 +70,60 @@ if ($data->flow == 'listCompany') {
       ]
     )
   );
-} else if ($data->flow == 'addCompany') {
-  $guid = GUID();
+} else if ($input->flow == 'getCompany') {
+  $id = $input->id;
+  $sql = "select id,name,account_name,account_pass,asq_total,asq_left,asq_se_total,asq_se_left,asq_se_2_total,asq_se_2_left from company where id='$id'";
 
-  $name = $data->name;
-  $account_name = $data->account_name;
-  $account_pass = $data->account_pass;
-  $asq_total = $data->asq_total;
-  $asq_left = $data->asq_left;
-  $asq_se_total = $data->asq_se_total;
-  $asq_se_left = $data->asq_se_left;
-  $asq_se_2_total  = $data->asq_se_2_total;
-  $asq_se_2_left  = $data->asq_se_2_left;
+  $result = $conn->query($sql);
+  if ($conn->affected_rows != 0) {
+    $model  = $result->fetch_assoc();
+    $model['sub_accounts'] = [];
+    $sql  = "select id,name,level,dept from sub_accounts where c_id ='$id'";
 
-  $sql = "insert into company (guid,name,account_name,account_pass,asq_total,asq_left,asq_se_total,asq_se_left,asq_se_2_total,asq_se_2_left) values ('$guid','$name','$account_name','$account_pass',$asq_total,$asq_left,$asq_se_total,$asq_se_left,$asq_se_2_total,$asq_se_2_left)";
+    $result = $conn->query($sql);
+
+    if ($conn->affected_rows != 0) {
+      while ($rs = $result->fetch_assoc()) {
+        array_push($model['sub_accounts'], $rs);
+      }
+    }
+    echo json_encode(
+      [
+        "faultCode" => 0,
+        'faultReason' => 'OK',
+        "data" =>  $model
+      ]
+    );
+  }
+} else if ($input->flow == 'addCompany') {
+
+  $name = $input->name;
+  $account_name = $input->account_name;
+  $account_pass = $input->account_pass;
+  $asq_total = $input->asq_total;
+  $asq_left = $input->asq_left;
+  $asq_se_total = $input->asq_se_total;
+  $asq_se_left = $input->asq_se_left;
+  $asq_se_2_total  = $input->asq_se_2_total;
+  $asq_se_2_left  = $input->asq_se_2_left;
+
+  $sql = "insert into company (name,account_name,account_pass,asq_total,asq_left,asq_se_total,asq_se_left,asq_se_2_total,asq_se_2_left) values ('$name','$account_name','$account_pass',$asq_total,$asq_left,$asq_se_total,$asq_se_left,$asq_se_2_total,$asq_se_2_left)";
 
   $result =   $conn->query($sql);
   if ($conn->affected_rows != 0) {
-    echo json_encode(
-      [
-        "FaultCode" => 0,
-        'FaultReason' => 'OK',
-      ]
-    );
+
+    $sql = "select id,name,account_name,asq_total,asq_left,asq_se_total,asq_se_left,asq_se_2_total,asq_se_2_left from company  where name='$name'";
+    $result2 = $conn->query($sql);
+    if ($conn->affected_rows != 0) {
+
+      $model  = $result2->fetch_assoc();
+      echo json_encode(
+        [
+          "faultCode" => 0,
+          'faultReason' => 'OK',
+          "data" =>  $model
+        ]
+      );
+    }
   }
 }
---
+++ backend/doctor.php
@@ -0,0 +1,1 @@
+<!-- if($REQUEST_METHOD == “POST”){ -->
