35fcf745128edafd4f80402149e372ee3b670b8e
panminxiang <1061886912@qq.com>
2022-08-26 18:42:18 +0800

    pmx

--- backend/inform.php
+++ backend/inform.php
@@ -26,8 +26,24 @@ if ($method == 'post') {
   $flow = $input->flow;
 
   if ($flow == 'listInforms') {
-  } else if ($flow == 'getInform') {
-    $sql = "SELECT * FROM `inform` WHERE id=(select max(id) from inform)";
+
+    $sql = "SELECT  id,content,create_time,update_time  FROM `inform`";
+    $result = $conn->query($sql);
+    $model = [];
+    if ($conn->affected_rows != 0) {
+      while ($tmp = $result->fetch_assoc()) {
+        array_push($model, $tmp);
+      }
+    }
+    echo json_encode(
+      [
+        "faultCode" => 0,
+        'faultReason' => 'OK',
+        "data" =>  $model
+      ]
+    );
+  } else if ($flow == 'getLatestInform') {
+    $sql = "SELECT id,content,create_time,update_time  FROM `inform` WHERE  is_latest='1'";
     $result = $conn->query($sql);
     $model = null;
     if ($conn->affected_rows != 0) {
@@ -41,6 +57,43 @@ if ($method == 'post') {
       ]
     );
   } else if ($flow == 'addInform') {
+
+    $id = GUID();
+    $content = $input->content;
+
+    $sql = "update inform set is_latest ='0'";
+
+    $conn->query($sql);
+
+    $sql = "insert into inform (id,content) values ('$id','$content')";
+    $conn->query($sql);
+    $sql  = "select id,content,create_time,update_time  from inform where id='$id'";
+    $result = $conn->query($sql);
+
+    if ($conn->affected_rows != 0) {
+      $model  =  $result->fetch_assoc();
+      echo json_encode(
+        [
+          "faultCode" => 0,
+          'faultReason' => 'OK',
+          'data' => $model
+        ]
+      );
+    }
+  } else if ($flow == 'deleteInform') {
+    $id = $input->id;
+
+
+    $sql = "update inform set is_latest ='0'";
+
+    $conn->query($sql);
+
+    echo json_encode(
+      [
+        "faultCode" => 0,
+        'faultReason' => 'OK',
+      ]
+    );
   }
 } else if ($method == 'get') {
 }
--- backend/orders.php
+++ backend/orders.php
@@ -25,7 +25,7 @@ if ($method == 'post') {
 
   $flow = $input->flow;
 
-  if ($flow == 'listOrders') {
+  if ($flow == 'listOrder') {
 
     $pageSize = $input->pageSize;
     $pageIndex = $input->pageIndex;
@@ -36,7 +36,7 @@ if ($method == 'post') {
     $start = ($pageIndex - 1) * $pageSize;
 
 
-    $sql = "select id,name,phone,order_type,price ,create_time,update_time from orders  where phone  like '%$phone%' limit $start,$pageSize";
+    $sql = "select id,name,phone,order_type,price ,create_time from orders  where phone  like '%$phone%' limit $start,$pageSize";
 
     $totalRecortCount = $conn->query("select count(*) from orders  where phone  like '%$phone%'")->fetch_assoc()['count(*)'];
 
@@ -74,17 +74,17 @@ if ($method == 'post') {
         ]
       )
     );
-  } else if ($flow == 'addDoctor') {
+  } else if ($flow == 'addOrder') {
     // $id = GUID();
     // $cid = $input->cid;
     // $name = $input->name;
     // $level = $input->level;
     // $dept = $input->dept;
     // $phone =  $input->phone;
-    // $sql = "insert into doctor (id,cid,name,level,dept,phone) values ('$id','$cid','$name','$level','$dept','$phone')";
+    // $sql = "insert into Order (id,cid,name,level,dept,phone) values ('$id','$cid','$name','$level','$dept','$phone')";
 
     // $conn->query($sql);
-    // $result = $conn->query("select id,cid,name,level,dept,create_time,update_time from doctor where id='$id'");
+    // $result = $conn->query("select id,cid,name,level,dept,create_time from Order where id='$id'");
 
     // if ($conn->affected_rows != 0) {
     //   $model  =  $result->fetch_assoc();
@@ -96,28 +96,43 @@ if ($method == 'post') {
     //     ]
     //   );
     // }
-  } else if ($flow == 'deleteDoctor') {
-    // $id = $input->id;
-    // $cid = $input->cid;
+  } else if ($flow == 'deleteOrder') {
+    $id = $input->id;
 
-    // $sql = "delete from doctor where id='$id'";
-    // $result = $conn->query($sql);
-    // echo json_encode(
-    //   [
-    //     "faultCode" => 0,
-    //     'faultReason' => 'OK',
-    //   ]
-    // );
-  } else if ($flow == 'editDoctor') {
+    $sql  = "select id,name,phone,order_type,price ,create_time from orders where id='$id' ";
+    $result = $conn->query($sql);
+
+    if ($conn->affected_rows != 0) {
+      $model = $result->fetch_assoc();
+
+      $sql = "delete from orders where id='$id'";
+      $result = $conn->query($sql);
+      echo json_encode(
+        [
+          "faultCode" => 0,
+          'faultReason' => 'OK',
+          "data" => $model
+        ]
+      );
+    } else {
+      echo json_encode(
+        [
+          "faultCode" => 0,
+          'faultReason' => 'OK',
+          "data" => $model
+        ]
+      );
+    }
+  } else if ($flow == 'editOrder') {
     // $id = $input->id;
     // $name = $input->name;
     // $level = $input->level;
     // $dept = $input->dept;
     // $phone =  $input->phone;
-    // $sql  = "update doctor set name='$name',level='$level',dept='$dept',phone='$phone' where id='$id'";
+    // $sql  = "update Order set name='$name',level='$level',dept='$dept',phone='$phone' where id='$id'";
     // $conn->query($sql);
 
-    // $result = $conn->query("select id,cid,name,level,dept,create_time,update_time from doctor where id='$id'");
+    // $result = $conn->query("select id,cid,name,level,dept,create_time from Order where id='$id'");
 
     // if ($conn->affected_rows != 0) {
     //   $model  =  $result->fetch_assoc();
@@ -129,25 +144,55 @@ if ($method == 'post') {
     //     ]
     //   );
     // }
+  } else if ($flow == 'exportOrder') {
+    $beginTime = getTime($input->beginTime);
+    $endTime = getTime($input->endTime);
+
+    $sql  = "select id,name,phone,order_type,price ,create_time from orders where create_time between '$beginTime' and '$endTime'";
+
+    $result = $conn->query($sql);
+    $Data = [];
+
+    if ($conn->affected_rows != 0) {
+      $RecordCount = $conn->affected_rows;
+      while ($rs = $result->fetch_assoc()) {
+        array_push($Data, $rs);
+      }
+    }
+
+
+    // foreach ($Data as &$value) {
+    //   $id = $value['id'];
+    //   $doctors = getDoctor($id);
+    //   $value['doctors'] = $doctors;
+    // }
+
+    echo json_encode(
+      array(
+        "faultCode" => 0,
+        'faultReason' => 'OK',
+        "data" => $Data
+      )
+    );
   }
 } else if ($method == 'get') {
   // $id = $_GET['id'];
-  // $doctor = getDoctor($id);
+  // $Order = getOrder($id);
 
   // echo json_encode(
   //   [
   //     "faultCode" => 0,
   //     'faultReason' => 'OK',
-  //     "data" =>  $doctor
+  //     "data" =>  $Order
   //   ]
   // );
 }
 
 
-function getDoctor(string $id)
+function getOrder(string $id)
 {
   global $conn;
-  $sql =  "select id,cid,name,level,dept,create_time,update_time from doctor where id='$id'";
+  $sql =  "select id,cid,name,level,dept,create_time from Order where id='$id'";
   $result = $conn->query($sql);
   if ($conn->affected_rows != 0) {
     return $result->fetch_assoc();
@@ -155,11 +200,11 @@ function getDoctor(string $id)
   return null;
 }
 
-function listDoctor(string $cid)
+function listOrder(string $cid)
 {
   global $conn;
   $data = [];
-  $sql = "select id,cid,name,level,dept,create_time,update_time from doctor where cid='$cid' ";
+  $sql = "select id,cid,name,level,dept,create_time from Order where cid='$cid' ";
   $result = $conn->query($sql);
   if ($conn->affected_rows != 0) {
     while ($tmp = $result->fetch_assoc()) {
