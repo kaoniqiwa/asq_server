d280a6558634c8a29168d1018422a8dca64b94c5
panminxiang <1061886912@qq.com>
2022-10-07 19:16:56 +0800

    getinfo.php

--- getInfo.php
+++ getInfo.php
@@ -74,6 +74,60 @@ if (!is_null($company)) {
 }
 
 
+/* if (!is_null($company)) {
+  $sql = "select * from baby where mid in (select id from member where did in (  select id from doctor where cid='$company[id]'))";
+  $result = $conn->query($sql);
+  $babys = [];
+  if ($conn->affected_rows != 0) {
+    while ($tmp = $result->fetch_assoc()) {
+      array_push($babys, $tmp);
+    }
+  }
+  for ($i = 0; $i < count($babys); $i++) {
+    $baby = &$babys[$i];
+
+    $sql = "select * from asq3_test where bid='$baby[id]'";
+    $asq3Test = [];
+    $result =  $conn->query($sql);
+    if ($conn->affected_rows != 0) {
+      while ($tmp = $result->fetch_assoc()) {
+        array_push($asq3Test, $tmp);
+      }
+    }
+    $baby['asq3Test'] = $asq3Test;
+
+    $sql = "select * from asqse_test where bid='$baby[id]'";
+    $asqseTest = [];
+    $result =  $conn->query($sql);
+    if ($conn->affected_rows != 0) {
+      while ($tmp = $result->fetch_assoc()) {
+        array_push($asqseTest, $tmp);
+      }
+    }
+    $baby['asqseTest'] = $asqseTest;
+
+    $sql = "select * from asqse2_test where bid='$baby[id]'";
+    $asqse2Test = [];
+    $result =  $conn->query($sql);
+    if ($conn->affected_rows != 0) {
+      while ($tmp = $result->fetch_assoc()) {
+        array_push($asqse2Test, $tmp);
+      }
+    }
+    $baby['asqse2Test'] = $asqse2Test;
+  }
+  echo json_encode(
+    [
+      "faultCode" => 0,
+      'faultReason' => 'OK',
+      'data' => $babys
+    ]
+  );
+} else {
+  die("未查询到该机构信息");
+} */
+
+
 
 function getCompany($username, $password)
 {
