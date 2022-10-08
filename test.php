2ec67f7d1ef9926cf6b7074033b6c85f1a0083aa
panminxiang <1061886912@qq.com>
2022-10-08 16:41:37 +0800

    pmx

--- test.php
+++ test.php
@@ -18,3 +18,17 @@ $arr = [
 $arr  = array_change_key_case($arr, CASE_LOWER);
 
 var_dump($arr);
+
+
+
+if (!function_exists('getallheaders')) {
+  function getallheaders()
+  {
+    foreach ($_SERVER as $name => $value) {
+      if (substr($name, 0, 5) == 'HTTP_') {
+        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+      }
+    }
+    return $headers;
+  }
+}
