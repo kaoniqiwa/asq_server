5afd5a8850efd1af0a8c29e66225fe7c36f0610c
panminxiang <1061886912@qq.com>
2022-10-18 21:51:39 +0800

    pmx

--- back_end.login.php
+++ back_end.login.php
@@ -3,6 +3,20 @@
 include('./utility/mysql.php');
 
 
+if (!function_exists('getallheaders')) {
+  function getallheaders()
+  {
+    $headers = array();
+    foreach ($_SERVER as $name => $value) {
+      if (substr($name, 0, 5) == 'HTTP_') {
+        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+      }
+    }
+    return $headers;
+  }
+}
+
+
 $realm = "neoballoon.com";
 
 $username = '';
